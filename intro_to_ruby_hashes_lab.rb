b254030b60437656478dfa6d3d8e50340c82cf1b
Theo Carney <theoluyi@gmail.com>
2019-12-04 01:39:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,6 @@ def monopoly_with_third_tier
   railroads: { 
     pieces: 4,
     rent_in_dollars: {:key => 'a', :key2 => 'b', :key3 => 'c', :key4 => 'd'}
-    :names => {:key5 => 'e', :key6 => 'f', :key7 => 'g', :key8 => 'h'}
     }
   }
   }
