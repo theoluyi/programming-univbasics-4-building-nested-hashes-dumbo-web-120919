3eee2126f3fdd54e8f6c9007d22aa1b38317f507
Theo Carney <theoluyi@gmail.com>
2019-12-04 01:38:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
     names: {:key5 => 'e', :key6 => 'f', :key7 => 'g', :key8 => 'h'}
     }
   }
-  
+  }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
