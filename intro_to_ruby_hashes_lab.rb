dc4fe816aba4c7014a6fc8018f4a42e1ae612fb2
Theo Carney <theoluyi@gmail.com>
2019-12-04 01:59:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,6 @@ def monopoly_with_fourth_tier
         :pennsylvania_railroad => {"mortgage_value" => '$200'}, 
         :b_and_o_railroad => {"mortgage_value" => '$400'}, 
         :shortline_railroad => {"mortgage_value" => '$800'}
-        
       },
       rent_in_dollars: {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
     }
