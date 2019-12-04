5c41135f1bb380c889ba6b3331666b30b389b112
Theo Carney <theoluyi@gmail.com>
2019-12-04 01:41:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   base_hash = {
     railroads: { 
       pieces: 4,
-      name: 'x'
+      name: 'x',
       rent_in_dollars: {:key => 'a', :key2 => 'b', :key3 => 'c', :key4 => 'd'}
     }
   }
